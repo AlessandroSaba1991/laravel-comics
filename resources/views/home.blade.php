8909b663b863daccd05da7288b4247c3f143b68b
AlessandroSaba1991 <sabaalessandro515@gmail.com>
2022-06-09 18:03:15 +0200

    add footer complete

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,4 +28,50 @@
         </div>
     </div>
 </section>
+<section class="merchandises">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="merchandise">
+                    <div class="img">
+                        <img src="{{asset('../img/buy-comics-digital-comics.png')}}" alt="digital comics" />
+                    </div>
+                    <div class="text">digital comics</div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="merchandise">
+                    <div class="img">
+                        <img src="{{asset('../img/buy-comics-merchandise.png')}}" alt="digital comics" />
+                    </div>
+                    <div class="text">digital comics</div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="merchandise">
+                    <div class="img">
+                        <img src="{{asset('../img/buy-comics-subscriptions.png')}}" alt="digital comics" />
+                    </div>
+                    <div class="text">digital comics</div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="merchandise">
+                    <div class="img">
+                        <img src="{{asset('../img/buy-comics-shop-locator.png')}}" alt="digital comics" />
+                    </div>
+                    <div class="text">digital comics</div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="merchandise">
+                    <div class="img">
+                        <img src="{{asset('../img/buy-dc-power-visa.svg')}}" alt="digital comics" />
+                    </div>
+                    <div class="text">digital comics</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 @endsection
