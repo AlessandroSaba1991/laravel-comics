e4bdeb6521fd170233bd90ea76cab0a360412b2e
AlessandroSaba1991 <sabaalessandro515@gmail.com>
2022-06-10 19:24:34 +0200

    add details of comic.show

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -46,7 +46,86 @@
         </div>
     </div>
 </section>
-<section class="details">
-
+<section class="details_comic py-4">
+    <div class="container sm_">
+        <div class="row">
+            <div class="col-6">
+                <div class="talent mb-5 pb-5">
+                    <h4>Talent</h4>
+                    <div class="detail">
+                        <div class="row">
+                            <div class="col-4">
+                                <p>Art By:</p>
+                            </div>
+                            <div class="col-8">
+                            <p>
+                                @forelse($comic['artists'] as $artist)
+                                <span>{{$artist}}</span>,
+                                @empty
+                                No Results
+                                @endforelse
+                            </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="detail">
+                        <div class="row">
+                            <div class="col-4">
+                                <p>Written By:</p>
+                            </div>
+                            <div class="col-8">
+                            <p>
+                                @forelse($comic['writers'] as $writer)
+                                <span>{{$writer}}</span>,
+                                @empty
+                                No Results
+                                @endforelse
+                            </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="specs">
+                    <h4>Specs</h4>
+                    <div class="detail">
+                        <div class="row">
+                            <div class="col-4">
+                                <p>Series:</p>
+                            </div>
+                            <div class="col-8">
+                            <p>
+                                <span class="text-uppercase">{{$comic['series']}}</span>
+                            </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="detail">
+                        <div class="row">
+                            <div class="col-4">
+                                <p>U.S. Price:</p>
+                            </div>
+                            <div class="col-8">
+                            <p>{{$comic['price']}}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="detail">
+                        <div class="row">
+                            <div class="col-4">
+                                <p>On Sale Date:</p>
+                            </div>
+                            <div class="col-8">
+                            <p>
+                                {{date_format(date_create_from_format("Y-m-d",$comic['sale_date']),"M d Y")}}
+                            </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
 @endsection
